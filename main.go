0389d20b76cc9876f2a991931bdcaa54cac6a0bf
Viktor Farcic <viktor@farcic.com>
2024-06-05 14:23:06 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Server
+	log.Println("Starting server...")
+	router := gin.New()
+	router.GET("/", rootHandler)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	router.Run(fmt.Sprintf(":%s", port))
+}
+
+func rootHandler(ctx *gin.Context) {
+	slog.Debug("Handling request", "URI", ctx.Request.RequestURI)
+	ctx.String(http.StatusOK, "This is a silly demo")
+}
